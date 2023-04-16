2f4db09ac4aaa527f48bfe9e0578620eb3f8a55c
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:16:45 +0530

    Copy Constructor in CPP

--- BSc2ndYear/copyConstructor.cpp
+++ BSc2ndYear/copyConstructor.cpp
@@ -1,1 +1,34 @@
+#include<iostream.h>
+#include<conio.h>
 
+class className{
+int a,b;
+public:
+
+className(int a,int b){
+this->a = a;
+this->b = b;
+
+}
+className(className  &obj){
+a = obj.a;
+b = obj.b;
+}
+
+
+void getValue(){
+cout<<"value of a = "<<a<<endl;
+cout<<"value of b = "<<b<<endl;
+}
+};
+
+main(){
+clrscr();
+className obj1(6,9),obj2(obj1);
+cout<<"obj1 "<<endl;
+obj1.getValue();
+cout<<endl<<"obj2"<<endl;
+obj2.getValue();
+
+getch();
+}
