ef30360fc20d48761181220f4f1e5359a6c11f3b
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:28:49 +0530

    Static Methods in CPP

--- BSc2ndYear/staticMethod.cpp
+++ BSc2ndYear/staticMethod.cpp
@@ -1,1 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
 
+class className{
+public:
+static int  x;
+
+static void getValue(){
+cout<<endl<<"value of x = "<<x++;
+}
+};
+int className::x = 10;
+
+main(){
+clrscr();
+
+className::getValue();
+className::getValue();
+className::getValue();
+getch();
+}
