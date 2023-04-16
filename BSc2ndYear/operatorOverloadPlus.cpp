928820d6b16368730883518d798ddefcd131c5b7
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:26:04 +0530

    Operator (Plus) Overloading in CPP

--- BSc2ndYear/operatorOverloadPlus.cpp
+++ BSc2ndYear/operatorOverloadPlus.cpp
@@ -1,1 +1,26 @@
+#include<iostream.h>
+#include<conio.h>
 
+class className{
+int x;
+public:
+void setValue(int a ){x =a ;}
+void getValue(){
+cout<<"x = "<<x<<endl;
+}
+
+className operator +(className obj){
+className temp;
+temp.x = x+obj.x;
+return temp;
+}
+};
+main(){
+clrscr();
+className obj1,obj2,obj3;
+obj1.setValue(60);
+obj2.setValue(9);
+obj3 = obj1+obj2;
+obj3.getValue();
+getch();
+}
