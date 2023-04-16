4d6488b8ed1491edc6135c704b94c7c218f972a6
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:20:39 +0530

    Update employee.cpp

--- BSc2ndYear/employee.cpp
+++ BSc2ndYear/employee.cpp
@@ -1,1 +1,33 @@
+#include<iostream.h>
+#include<conio.h>
 
+class company{
+char name[100];
+float pay,hra;
+public:
+company(){
+
+cout<<"Enter name : ";
+cin>>name;
+cout<<"Enter Basic Pay : ";
+cin>>pay;
+cout<<"Enter HRA : ";
+cin>>hra;
+}
+
+void details(){
+
+   cout<<endl<<"Name = "<<name;
+   cout<<endl<<"Basic Pay = "<<pay;
+   cout<<endl<<"HRA = "<<hra;
+   }
+
+};
+
+main(){
+clrscr();
+company ap;
+ap.details();
+
+getch();
+}
