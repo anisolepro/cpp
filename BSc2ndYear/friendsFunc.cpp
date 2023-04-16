0162045bb41cfa4c4423fc5b65abb50a57f6a439
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:29:50 +0530

    Find Sum of 2 numbers with friends Function in CPP

---
+++ BSc2ndYear/friendsFunc.cpp
@@ -0,0 +1,25 @@
+#include<iostream.h>
+#include<conio.h>
+
+class className{
+float a,b;
+public:
+void setValue(){
+cout<<"Enter 2 Numbers : ";
+cin>>a>>b;
+}
+friend void sum(className obj);
+};
+
+void sum(className obj) {
+cout<<endl<<"sum = "<<obj.a+obj.b;
+}
+
+main(){
+clrscr();
+className obj;
+obj.setValue();
+sum(obj);
+
+getch();
+}
