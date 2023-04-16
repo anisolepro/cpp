b6714783d786db178a201b07bc1830d977119449
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:20:03 +0530

    New and Delete Keyword for Memory Allocation in cpp

--- BSc2ndYear/dynamicAllocation.cpp
+++ BSc2ndYear/dynamicAllocation.cpp
@@ -1,1 +1,26 @@
+#include <iostream.h>
+#include <conio.h>
 
+main()
+{
+    int size, *arr;
+    clrscr();
+    cout << "Enter Length of Array : ";
+    cin >> size;
+
+    arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter Element " << i + 1 << " : ";
+        cin >> arr[i];
+    }
+
+    cout << "Array Elements : " << endl;
+    for (i = 0; i < size; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    delete[] arr;
+    getch();
+}
