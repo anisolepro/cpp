a5608ef7e2ec93c4db1a9d8215e05cac70de59e7
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:31:25 +0530

    Find Sum Of Two Numbers With friends Function in CPP

--- BSc2ndYear/friendsFunc.cpp
+++ BSc2ndYear/friendsFunc.cpp
@@ -1,25 +1,32 @@
-#include<iostream.h>
-#include<conio.h>
+#include <iostream.h>
+#include <conio.h>
 
-class className{
-float a,b;
+class className
+{
+    float a, b;
+
 public:
-void setValue(){
-cout<<"Enter 2 Numbers : ";
-cin>>a>>b;
-}
-friend void sum(className obj);
+    void setValue()
+    {
+        cout << "Enter 2 Numbers : ";
+        cin >> a >> b;
+    }
+    // friends Function can access private properties and Methods
+    friend void sum(className obj);
 };
 
-void sum(className obj) {
-cout<<endl<<"sum = "<<obj.a+obj.b;
+void sum(className obj)
+{
+    cout << endl
+         << "sum = " << obj.a + obj.b;
 }
 
-main(){
-clrscr();
-className obj;
-obj.setValue();
-sum(obj);
+main()
+{
+    clrscr();
+    className obj;
+    obj.setValue();
+    sum(obj);
 
-getch();
+    getch();
 }
