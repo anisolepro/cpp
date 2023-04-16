fa31f7b83cd81db639a20c53d9034634ab394207
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:25:19 +0530

    Operator (Minus) OverLoading in CPP

--- BSc2ndYear/operatorOverloadMinus.cpp
+++ BSc2ndYear/operatorOverloadMinus.cpp
@@ -1,1 +1,29 @@
+#include<iostream.h>
+#include<conio.h>
 
+class className{
+int x ;
+public:
+void setValue(int a){ x =a;}
+void getValue(){
+cout<<"x = "<<x<<endl;
+}
+
+className operator -(){
+className temp;
+temp.x = -x;
+return temp;
+}
+};
+
+main(){
+clrscr();
+className obj1;
+obj1.setValue(69);
+cout<<"Before operator overLoad "<<endl;
+obj1.getValue();
+obj1 = -obj1;
+cout<<"After Operator overload "<<endl;
+obj1.getValue();
+getch();
+}
