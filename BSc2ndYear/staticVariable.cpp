28f115abce8968c6f3d7d16ead546dc89348714b
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:27:50 +0530

    Update staticVariable.cpp

--- BSc2ndYear/staticVariable.cpp
+++ BSc2ndYear/staticVariable.cpp
@@ -1,1 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
 
+class className{
+public :
+static int count;
+void getvalue(){
+cout<<"cout Value = "<<count++<<endl;
+}
+};
+int className::count = 0;
+
+
+main() {
+clrscr();
+className obj1,obj2;
+ obj1.getvalue();
+ obj2.getvalue();
+ obj1.getvalue();
+getch();
+}
