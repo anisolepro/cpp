c16c83bb34771e6a15b2f3c26c5edcb9f3f864a3
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:22:05 +0530

    Find Largerst Number In CPP

--- BSc2ndYear/largestNumb.cpp
+++ BSc2ndYear/largestNumb.cpp
@@ -1,1 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
 
+class className{
+float a,b,c,big;
+public:
+void largeNumb(){
+cout<<"\nEnter 3 numbers : ";
+cin>>a>>b>>c;
+big = b>a?b:a;
+big = c>big ? c:big;
+cout<<big<<" is the greatest Number";
+}
+};
+main(){
+clrscr();
+className obj;
+obj.largeNumb();
+getch();
+}
