e4d0a5fc83e537e50e94b2b5feb5cad768be362e
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:21:13 +0530

    Inline Function in CPP

--- BSc2ndYear/inlineFunc.cpp
+++ BSc2ndYear/inlineFunc.cpp
@@ -1,1 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
 
+inline float area(float radius){
+return 3.142*radius*radius  ;
+}
+
+main(){
+float radius;
+clrscr();
+cout<<"Enter radius :";
+cin>>radius;
+cout<<"Area = "<<area(radius);
+
+getch();
+}
